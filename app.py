d764813812c0130bce08ad059c4c81423d11ce96
Guillermo <reyes.g.r.suarez@gmail.com>
2025-06-11 11:33:15 -0500

    Segundo intento

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import numpy as np
 import tensorflow as tf
 import psycopg2
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
 
 
-modelo = tf.keras.models.load_model('modelo_entrenado_final.h5')
+modelo = tf.keras.models.load_model('modeloDEC.h5')
+model = tf.keras.models.load_model('modeloDEC') 
 
 def get_db_connection():
-    return psycopg2.connect('postgresql://db_unfv_ver5_user:rTxeXCWafkztYkNnhrRPZCnBIqATGP1c@dpg-d13fbvk9c44c7399ca1g-a.oregon-postgres.render.com/db_unfv_ver5')
+    return psycopg2.connect(os.getenv('DATABASE_URL'))
+    #return psycopg2.connect('postgresql://db_unfv_ver5_user:rTxeXCWafkztYkNnhrRPZCnBIqATGP1c@dpg-d13fbvk9c44c7399ca1g-a.oregon-postgres.render.com/db_unfv_ver5')
 
 @app.route('/')
 def home():
